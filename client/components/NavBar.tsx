fd0b738e07ba670dc0977a0f6a4b1df837cf62b0
Zeno3463 <zenolim3463@gmail.com>
2022-03-06 10:44:20 +0800

    feat: Added IPFS

--- client/components/NavBar.tsx
+++ client/components/NavBar.tsx
@@ -7,9 +7,9 @@ const NavBar = () => {
   return (
 		<div className='flex justify-between shadow-2xl rounded-full mx-10 my-5'>
 			<div className='flex p-3'>
-				<Link href="/"><Image src={Logo} width={60} height={60} /></Link>
-				<Link href="/explore"><p className='m-auto px-3 font-bold'>Explore</p></Link>
-				<Link href="/about"><p className='m-auto px-3 font-bold'>About</p></Link>
+				<Link href="/"><Image src={Logo} className='hover:cursor-pointer' width={60} height={60} /></Link>
+				<Link href="/explore"><p className='m-auto px-3 font-bold hover:cursor-pointer'>Explore</p></Link>
+				<Link href="/about"><p className='m-auto px-3 font-bold hover:cursor-pointer'>About</p></Link>
 			</div>
 			<div className='flex px-10 py-5'>
 				<button className='px-5 gradient-button'>Login</button>
--- client/pages/newPage.tsx
+++ client/pages/newPage.tsx
@@ -3,21 +3,29 @@ import NavBar from '../components/NavBar'
 import dynamic from 'next/dynamic';
 const ReactQuill = dynamic(() => import('react-quill'), {ssr: false});
 import 'react-quill/dist/quill.snow.css'
+import * as IPFS from 'ipfs-core';
 
 const NewPage = () => {
 	const [title, setTitle] = useState("");
 	const [description, setDescription] = useState('');
 
+	const launchPage = async () => {
+		const ipfs = await IPFS.create();
+		const { cid } = await ipfs.add(JSON.stringify({title, description}));
+		const hash = Array.from(cid['_baseCache'].values())[0];
+		// TODO: store the hash in the smart contract
+	}
+
 	return (
 		<>
 			<NavBar />
 			<div className='flex flex-col m-10'>
-				<input type="Text" placeholder='title' className='shadow-2xl my-5 p-3 w-1/2 rounded-lg outline-none' />
+				<input type="Text" placeholder='title' className='shadow-2xl my-5 p-3 w-1/2 rounded-lg outline-none' value={title} onChange={e => setTitle(e.target.value)} />
 				<div className='shadow-2xl my-5 p-3 rounded-lg outline-none'>
 					<ReactQuill value={description} onChange={setDescription} />
 				</div>
 				<div>
-					<button className='py-3 px-5 gradient-button my-5'>Launch!</button>
+					<button className='py-3 px-5 gradient-button my-5' onClick={launchPage}>Launch!</button>
 				</div>
 			</div>
 		</>
