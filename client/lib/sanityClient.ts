76b9d76b86040f55713524a498cd0aa923174d37
Zeno3463 <zenolim3463@gmail.com>
2022-03-11 12:41:42 +0800

    feat: Added sanity

--- client/lib/sanityClient.ts
+++ client/lib/sanityClient.ts
@@ -1,9 +1,9 @@
 import sanityClient from '@sanity/client';
 
 export const client = sanityClient({
-	projectId: 'n5xwp5dk',
+	projectId: 'zw3hbu5b',
 	dataset: 'production',
 	apiVersion: 'v1',
-	token: 'sk563dVGNwW69IuE7slDyikXnWWb7K1eqJFy6GRBhMlZXttC3DRDzBlsbmKtkP07InEpRIZyql4m1e1b0hTmni2sOynZFIQxFxGZZbabJ00QpjIR39PaViR1yN71OZ3IllZdgLOpW8T6qBm4nULgzXCti4YZdcjjmFqU6XT9DKGxaaPHQ9aZ',
+	token: 'skfLliLL5bOfoNH5gZb3QsE78K8zDoxDY1qew8s9YeAhFCEeJ2kD8FcJiAMLzmaOudwmK5TcjQTzqUADHBARiWKRdxyL5pL65ssLPrCpKQzJfrIbDoqxyJ2TIPHxj6Djm1swAUY0X6Cto1O8kuI3xyNqSRSfon4pMw9dUatAmhUVQ1ue8OCS',
 	useCdn: false
 })
