e37aa44f71ed9e99040932df66075119ef4cfad9
Zeno3463 <zenolim3463@gmail.com>
2022-03-15 07:54:39 +0800

    revert: Added .env

--- client/lib/constants.ts
+++ client/lib/constants.ts
@@ -1,5 +1,4 @@
 import abi from './Transaction.json';
 
 export const transactionAbi = abi.abi;
-// TX_ADDR
-export const transactionAddress = process.env.TX_ADDR;
+export const transactionAddress = "0xA706F36B74833Aca821eA4C993c765cB66376A76";
--- client/lib/sanityClient.ts
+++ client/lib/sanityClient.ts
@@ -4,7 +4,6 @@ export const client = sanityClient({
 	projectId: 'zw3hbu5b',
 	dataset: 'production',
 	apiVersion: 'v1',
-	// SANITY_TOKEN
-	token: process.env.SANITY_TOKEN,
+	token: "skfLliLL5bOfoNH5gZb3QsE78K8zDoxDY1qew8s9YeAhFCEeJ2kD8FcJiAMLzmaOudwmK5TcjQTzqUADHBARiWKRdxyL5pL65ssLPrCpKQzJfrIbDoqxyJ2TIPHxj6Djm1swAUY0X6Cto1O8kuI3xyNqSRSfon4pMw9dUatAmhUVQ1ue8OCS",
 	useCdn: false
 })
